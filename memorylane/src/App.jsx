6c9195ef2b598f0d3f57b1f253fb995adea423e7
Amol Venkataraman <amolven16@gmail.com>
2024-01-27 14:15:00 -0500

    Some changes

--- memorylane/src/App.jsx
+++ memorylane/src/App.jsx
@@ -16,26 +16,26 @@ function App() {
         </div>
         <div className="page-content">
           <div className="left-pane">
-            <div className="image-container">
-              <img src="https://plus.unsplash.com/premium_photo-1705418057300-84235e5f265a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <div className="image-container fullwidth">
+              <img className="fullwidth rounded" src="https://plus.unsplash.com/premium_photo-1705418057300-84235e5f265a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             </div>
-            <div className="hints-container">
+            <div className="hints-container fullwidth ">
 
             </div>
           </div>
           <div className="right-pane">
-            <div className="solution-selectors">
-              <div className="map-container">
+            <div className="solution-selectors fullwidth">
+              <div className="map-container fullwidth">
                 <div id="map"></div>
               </div>
-              <div className="date-selector">
+              <div className="date-selector fullwidth">
 
               </div>
-              <div className="time-selector">
+              <div className="time-selector fullwidth">
 
               </div>
             </div>
-            <button className="submit-guess">
+            <button className="submit-guess fullwidth rounded">
 
             </button>
           </div>
