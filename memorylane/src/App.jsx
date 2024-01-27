6264a9f84eca3c024faa56567970b02d33de6628
Amol Venkataraman <amolven16@gmail.com>
2024-01-27 11:52:45 -0500

    Added basic UI. Still WIP

--- memorylane/src/App.jsx
+++ memorylane/src/App.jsx
@@ -8,26 +8,39 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-content">
+        <div className="navbar">
+          <div className="navbar-badge">
+            MemoryLane
+          </div>
+        </div>
+        <div className="page-content">
+          <div className="left-pane">
+            <div className="image-container">
+              <img src="https://plus.unsplash.com/premium_photo-1705418057300-84235e5f265a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            </div>
+            <div className="hints-container">
+
+            </div>
+          </div>
+          <div className="right-pane">
+            <div className="solution-selectors">
+              <div className="map-container">
+                <div id="map"></div>
+              </div>
+              <div className="date-selector">
+
+              </div>
+              <div className="time-selector">
+
+              </div>
+            </div>
+            <button className="submit-guess">
+
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
