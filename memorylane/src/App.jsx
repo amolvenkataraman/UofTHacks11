da09d7a205e09731e025688a666c504a59f19085
Hetarth Mahida <hetarthmahida@gmail.com>
2024-01-28 05:07:48 -0500

    socre logic update

--- memorylane/src/App.jsx
+++ memorylane/src/App.jsx
@@ -1,16 +1,43 @@
+import { useState } from 'react'
 import Navbar from './components/navbar'
 import LeftPane from './components/leftpane'
 import RightPane from './components/rightpane'
+import axios from "axios";
 import './App.css'
 
+var config = { headers: {  
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*'}
+}
+
+function score(d, t) {	// d = distance, t = time
+  const maxScore = 5000;
+
+  const distOne = 200000;
+  const timeOne = 1826;
+  
+  const distC = Math.log(maxScore) / distOne;
+  const timeC = Math.log(maxScore) / timeOne;
+
+  const distScore = Math.exp(-distC * d);
+  const timeScore = Math.exp(-timeC * t);
+
+  return Math.trunc(distScore * timeScore * maxScore);
+}
+
+let finalScore = score(10000, 7);
+
+
 function App() {
+  //const [url, seturl] = useState(""); // Sets the url to the image
+  let url="This is the URL"
 
   return (
     <>
       <div className="main-content">
-        <Navbar />
+        <Navbar finalScore= { finalScore }/>
         <div className="page-content">
-          <LeftPane />
+          <LeftPane url= { url } />
           <RightPane />
         </div>
       </div>
